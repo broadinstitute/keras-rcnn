68d5af48b099b4e5c99c7a96c9c5392997c86ed8
Allen Goodman <allen.goodman@icloud.com>
2017-05-25 18:08:13 -0400

    anchor layer

--- keras_rcnn/layers/object_detection.py
+++ keras_rcnn/layers/object_detection.py
@@ -3,6 +3,45 @@ import keras.engine.topology
 import keras_rcnn.backend
 
 
+class Anchor(keras.engine.topology.Layer):
+    """
+    Assign anchors to ground-truth targets.
+    
+    It produces:
+        1. anchor classification labels
+        2. bounding-box regression targets.
+    """
+    def __init__(self, stride=16, scales=None, ratios=None, **kwargs):
+        if ratios is None:
+            self.ratios = [0.5, 1, 2]
+        else:
+            self.ratios = ratios
+
+        if scales is None:
+            scales = [8, 16, 32]
+        else:
+            self.scales = scales
+
+        self.output_dim = (None, None, 4)
+
+        self.ratios = ratios
+
+        self.scales = scales
+
+        self.stride = stride
+
+        super(Anchor, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        super(Anchor, self).build(input_shape)
+
+    def call(self, inputs, **kwargs):
+        return inputs
+
+    def compute_output_shape(self, input_shape):
+        return self.output_dim
+
+
 class ObjectProposal(keras.engine.topology.Layer):
     """
     Transforms RPN outputs (per-anchor scores and bounding box regression estimates) into object proposals.
