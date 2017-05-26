7cbfe4ac6b863ad3ed1a844fdf981ec14b1523d8
Allen Goodman <allen.goodman@icloud.com>
2017-05-26 15:47:01 -0400

    bbox_transform backend function

--- keras_rcnn/backend/common.py
+++ keras_rcnn/backend/common.py
@@ -2,6 +2,29 @@ import keras.backend
 import numpy
 
 
+def bbox_transform(ex_rois, gt_rois):
+    ex_widths = ex_rois[:, 2] - ex_rois[:, 0] + 1.0
+    ex_heights = ex_rois[:, 3] - ex_rois[:, 1] + 1.0
+    ex_ctr_x = ex_rois[:, 0] + 0.5 * ex_widths
+    ex_ctr_y = ex_rois[:, 1] + 0.5 * ex_heights
+
+    gt_widths = gt_rois[:, 2] - gt_rois[:, 0] + 1.0
+    gt_heights = gt_rois[:, 3] - gt_rois[:, 1] + 1.0
+    gt_ctr_x = gt_rois[:, 0] + 0.5 * gt_widths
+    gt_ctr_y = gt_rois[:, 1] + 0.5 * gt_heights
+
+    targets_dx = (gt_ctr_x - ex_ctr_x) / ex_widths
+    targets_dy = (gt_ctr_y - ex_ctr_y) / ex_heights
+    targets_dw = keras.backend.log(gt_widths / ex_widths)
+    targets_dh = keras.backend.log(gt_heights / ex_heights)
+
+    targets = keras.backend.stack((targets_dx, targets_dy, targets_dw, targets_dh))
+
+    targets = keras.backend.transpose(targets)
+
+    return targets
+
+
 def clip(boxes, shape):
     proposals = [
         keras.backend.maximum(keras.backend.minimum(boxes[:, 0::4], shape[1] - 1), 0),
