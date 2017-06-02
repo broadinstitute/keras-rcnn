3b8defeff8108a7dde11f8e5981b47939c2219e3
jihongju <daniel.jihong.ju@gmail.com>
2017-06-02 21:55:54 +0200

    Add R-CNN classification and regression losses

--- tests/test_losses.py
+++ tests/test_losses.py
@@ -21,3 +21,19 @@ def test_rpn_regression():
     expected_loss = np.power(0.5, 3)
     loss = K.eval(rpn_reg(y_true, y_pred))
     assert np.isclose(expected_loss, loss)
+
+
+def test_rcnn_classification():
+    y_pred = K.variable(np.random.random((1, 4, 4)))
+    y_true = K.variable(np.ones((1, 4, 4)))
+    assert K.eval(losses.rcnn_classification(y_true, y_pred)).shape == ()
+
+
+def test_rcnn_regression():
+    n_classes = 9
+    rcnn_reg = losses.rcnn_regression(n_classes)
+    y_pred = K.variable(0.5 * np.ones((1, 4, 4 * n_classes)))
+    y_true = K.variable(np.ones((1, 4, 8 * n_classes)))
+    expected_loss = np.power(0.5, 3)
+    loss = K.eval(rcnn_reg(y_true, y_pred))
+    assert np.isclose(expected_loss, loss)
