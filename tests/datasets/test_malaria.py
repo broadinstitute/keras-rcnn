b20cfae5dd2f30901f2d56e9cfdcf354acd400dd
jhung0 <jyhung@broadinstitute.org>
2017-06-04 21:55:06 -0400

    tests

--- tests/datasets/test_malaria.py
+++ tests/datasets/test_malaria.py
@@ -1,6 +1,7 @@
 import keras_rcnn.datasets.malaria
 
 def test_load_data():
-    training, test = malaria.load_data()
+    training, test = keras_rcnn.datasets.malaria.load_data()
+
     assert len(training) == 1208
     assert len(test) == 120
