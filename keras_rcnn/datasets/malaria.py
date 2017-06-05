4d979bc6a6db9521e4db4892878db6d36a44f0da
jhung0 <jyhung@broadinstitute.org>
2017-06-04 20:45:29 -0400

    load data documentation

--- keras_rcnn/datasets/malaria.py
+++ keras_rcnn/datasets/malaria.py
@@ -5,6 +5,10 @@ import keras.utils.data_utils
 
 
 def load_data():
+    """Loads the malaria dataset.
+    # Returns
+        Tuple of list of dictionaries where each dictionary corresponds to an image: `training, test`.
+    """
     origin = "http://keras-rcnn.storage.googleapis.com/malaria.tar.gz"
 
     pathname = keras.utils.data_utils.get_file("malaria", origin=origin, untar=True)
