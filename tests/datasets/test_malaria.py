47114b709978f8c5d00abd763dca20e03a81d524
jhung0 <jyhung@broadinstitute.org>
2017-06-05 10:31:06 -0400

    adding filename tests

--- tests/datasets/test_malaria.py
+++ tests/datasets/test_malaria.py
@@ -1,7 +1,9 @@
 import keras_rcnn.datasets.malaria
+import os.path
 
 def test_load_data():
     training, test = keras_rcnn.datasets.malaria.load_data()
-
+    assert os.path.exists(training[0]['filename'])
+    assert os.path.exists(test[0]['filename'])
     assert len(training) == 1208
     assert len(test) == 120
