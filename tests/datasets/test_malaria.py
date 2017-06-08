2035bbe85429d89441bbaba8a1e3a61782eedb8e
Allen Goodman <allen.goodman@icloud.com>
2017-06-08 13:05:24 -0400

    Create test_malaria.py

--- tests/datasets/test_malaria.py
+++ tests/datasets/test_malaria.py
@@ -1,9 +1,14 @@
-import keras_rcnn.datasets.malaria
 import os.path
+
+import keras_rcnn.datasets.malaria
 
 def test_load_data():
     training, test = keras_rcnn.datasets.malaria.load_data()
+    
     assert os.path.exists(training[0]['filename'])
+    
     assert os.path.exists(test[0]['filename'])
+    
     assert len(training) == 1208
+    
     assert len(test) == 120
