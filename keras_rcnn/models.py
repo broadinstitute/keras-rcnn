3efbb2bab85d60cd94f62986a84952aa8497f08c
JihongJu <jujihong.cn@gmail.com>
2017-06-10 00:52:18 +0200

    Add Faster R-CNN with Resnet.

--- keras_rcnn/models.py
+++ keras_rcnn/models.py
@@ -1,33 +1,61 @@
 # -*- coding: utf-8 -*-
 
 import keras
+import keras_resnet
 import keras_rcnn.layers
 
 
 class RCNN(keras.models.Model):
-    def __init__(self, inputs, classes, regions_of_interest):
-        y = keras_rcnn.layers.ROI(14, regions_of_interest)(inputs)
+    """
+    Faster R-CNN model by S Ren et, al. (2015).
 
-        y = keras.layers.Dense(4096)(y)
-        y = keras.layers.TimeDistributed(y)
+    :param inputs: input tensor (e.g. an instance of `keras.layers.Input`)
+    :param classes: integer, number of classes
+    :param rois: integer, number of regions of interest per image
+    """
 
-        score = keras.layers.Dense(classes)(y)
-        score = keras.layers.Activation("softmax")(score)
-        score = keras.layers.TimeDistributed(score)
+    def __init__(self, inputs, classes, rois):
+        if keras.backend.image_data_format() == "channels_last":
+            axis = 3
+        else:
+            axis = 1
+        y = keras_resnet.ResNet50(inputs)
+        features = y.layers[-2].output
 
-        boxes = keras.layers.Dense(4 * (classes - 1))(y)
-        boxes = keras.layers.Activation("linear")(boxes)
-        boxes = keras.layers.TimeDistributed(boxes)
+        rpn_classification = keras.layers.Conv2D(
+            9 * 1, (1, 1), activation="sigmoid")(features)
+        rpn_regression = keras.layers.Conv2D(9 * 4, (1, 1))(features)
 
-        super(RCNN, self).__init__(inputs, [score, boxes])
+        rpn_prediction = keras.layers.concatenate(
+            [rpn_classification, rpn_regression])
 
+        proposals = keras_rcnn.layers.object_detection.ObjectProposal(
+            rois)([rpn_classification, rpn_regression])
 
-class RPN(keras.models.Model):
-    def __init__(self, inputs, anchors):
-        y = keras.layers.Conv2D(512, (3, 3), activation="relu", padding="same")(inputs)
+        y = keras_rcnn.layers.ROI((7, 7), rois)([y, proposals])
 
-        score = keras.layers.Conv2D(anchors, (1, 1), activation="sigmoid")(y)
+        y = keras.layers.TimeDistributed(
+            keras.layers.Conv2D(1024, (1, 1)))(y)
 
-        boxes = keras.layers.Conv2D(anchors * 4, (1, 1))(y)
+        # res5 as in Mask R-CNN
+        block = keras_resnet.block.bottleneck
+        for i in range(3):
+            y = keras.layers.TimeDistributed(
+                block(512, (1, 1), i == 0))(y)
 
-        super(RPN, self).__init__(inputs, [score, boxes])
+        y = keras.layers.TimeDistributed(
+            keras.layers.BatchNormalization(axis=axis))(y)
+        y = keras.layers.TimeDistributed(
+            keras.layers.Activation("relu"))(y)
+
+        y = keras.layers.TimeDistributed(
+            keras.layers.AveragePooling2D((7, 7)))(y)
+
+        score = keras.layers.TimeDistributed(
+            keras.layers.Dense(classes, activation="softmax"))(y)
+
+        boxes = keras.layers.TimeDistributed(
+            keras.layers.Dense(4 * classes))(y)
+
+        super(RCNN, self).__init__(inputs,
+                                   [rpn_prediction, score, boxes])
