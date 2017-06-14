93f81c4ec6f21ed3cc63f92c2a85b7b0f7530791
Allen Goodman <allen.goodman@icloud.com>
2017-06-14 17:25:43 -0400

    pascal dataset

--- tests/datasets/test_malaria.py
+++ tests/datasets/test_malaria.py
@@ -2,13 +2,14 @@ import os.path
 
 import keras_rcnn.datasets.malaria
 
+
 def test_load_data():
     training, test = keras_rcnn.datasets.malaria.load_data()
-    
+
     assert os.path.exists(training[0]['filename'])
-    
+
     assert os.path.exists(test[0]['filename'])
-    
+
     assert len(training) == 1208
-    
+
     assert len(test) == 120
