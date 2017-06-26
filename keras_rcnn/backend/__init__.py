07244707a74ad207a1b61497011b454cc3ade192
Allen Goodman <allen.goodman@icloud.com>
2017-06-26 19:00:16 -0400

    cntk backend

--- keras_rcnn/backend/__init__.py
+++ keras_rcnn/backend/__init__.py
@@ -1,8 +1,27 @@
+import os
+
 from .common import *
 
-if keras.backend.backend() == "theano":
+_BACKEND = "tensorflow"
+
+if "KERAS_BACKEND" in os.environ:
+    _backend = os.environ["KERAS_BACKEND"]
+
+    backends = {
+        "cntk",
+        "tensorflow",
+        "theano"
+    }
+
+    assert _backend in backends
+
+    _BACKEND = _backend
+
+if _BACKEND == "cntk":
+    from .cntk_backend import *
+elif _BACKEND == "theano":
     from .theano_backend import *
-elif keras.backend.backend() == "tensorflow":
+elif _BACKEND == "tensorflow":
     from .tensorflow_backend import *
 else:
-    pass
+    raise ValueError("Unknown backend: " + str(_BACKEND))
