9aadff04d004ec2abeb7dcc68b46114263bab649
jihongju <daniel.jihong.ju@gmail.com>
2017-07-04 13:22:59 +0200

    Abstract the RCNN model and add ResNet50RCNN

--- keras_rcnn/models.py
+++ keras_rcnn/models.py
@@ -11,17 +11,20 @@ class RCNN(keras.models.Model):
     Faster R-CNN model by S Ren et, al. (2015).
 
     :param inputs: input tensor (e.g. an instance of `keras.layers.Input`)
-    :param classes: integer, number of classes
+    :param encoder: (Convolutional) feature extractor,
+        e.g., `keras_resnet.ResNet50`
+    :param heads: R-CNN heads for object detection and/or segmentation
+        on the proposed regions
     :param rois: integer, number of regions of interest per image
 
     :return model: a functional model API for R-CNN.
     """
 
-    def __init__(self, inputs, classes, rois):
-        # ResNet50 as body
-        y = keras_resnet.ResNet50(inputs)
+    def __init__(self, inputs, encoder, heads, rois):
+        # Extract features with the encoder
+        y = encoder(inputs)
         features = y.layers[-2].output
-
+        # Propose regions given the features
         rpn_classification = keras.layers.Conv2D(
             9 * 1, (1, 1), activation="sigmoid")(features)
         rpn_regression = keras.layers.Conv2D(9 * 4, (1, 1))(features)
@@ -31,13 +34,35 @@ class RCNN(keras.models.Model):
 
         proposals = keras_rcnn.layers.object_detection.ObjectProposal(
             rois)([rpn_regression, rpn_classification])
-
+        # Apply the heads on the proposed regions
         slices = keras_rcnn.layers.ROI((7, 7))([inputs, proposals])
 
-        [score, boxes] = keras_rcnn.heads.ResHead(classes)(slices)
+        [score, boxes] = heads(slices)
 
         super(RCNN, self).__init__(inputs,
                                    [rpn_prediction, score, boxes])
+
+
+class ResNet50RCNN(RCNN):
+    """
+    Faster R-CNN model with ResNet50.
+
+    :param inputs: input tensor (e.g. an instance of `keras.layers.Input`)
+    :param classes: integer, number of classes
+    :param rois: integer, number of regions of interest per image
+
+    :return model: a functional model API for R-CNN.
+    """
+
+    def __init__(self, inputs, classes, rois=300):
+        # ResNet50 as encoder
+        encoder = keras_resnet.ResNet50
+
+        # ResHead with score and boxes
+        heads = keras_rcnn.heads.ResHead(classes)
+
+        super(ResNet50RCNN, self).__init__(inputs, encoder, heads, rois)
+
 
 class RPN(keras.models.Model):
     def __init__(self, inputs):
--- tests/test_models.py
+++ tests/test_models.py
@@ -4,6 +4,6 @@ import keras_rcnn.models
 
 def test_resnet50_rcnn():
     inputs = keras.layers.Input((224, 224, 3))
-    model = keras_rcnn.models.RCNN(inputs, 21, 300)
+    model = keras_rcnn.models.ResNet50RCNN(inputs, 21, 300)
     model.compile(loss=["mse", "mse", "mse"],
                   optimizer="adam")
