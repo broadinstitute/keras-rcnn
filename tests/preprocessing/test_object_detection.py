abef75ab2cd3bd81ce80a50ab08aa7b336764a46
Mihai Morariu <m.a.morariu@delftrobotics.com>
2017-08-22 11:50:44 +0200

    Add unit test for 'scale_size'.

--- tests/preprocessing/test_object_detection.py
+++ tests/preprocessing/test_object_detection.py
@@ -5,12 +5,12 @@ import numpy
 def test_scale_shape():
     min_size = 200
     max_size = 300
-    shape    = (600, 1000, 3)
+    size     = (600, 1000)
 
-    shape, scale = keras_rcnn.preprocessing._object_detection.scale_shape(shape, min_size, max_size)
+    size, scale = keras_rcnn.preprocessing._object_detection.scale_size(size, min_size, max_size)
 
-    expected = (180, 300, 3)
-    numpy.testing.assert_equal(shape, expected)
+    expected = (180, 300)
+    numpy.testing.assert_equal(size, expected)
 
     expected = 0.3
     assert numpy.isclose(scale, expected)
