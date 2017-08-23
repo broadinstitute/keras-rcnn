4ef78c6c8a016393bc9c9cf34253cfed7db3842e
jhung0 <jyhung@broadinstitute.org>
2017-08-23 18:44:44 -0400

    layer for final bounding box outputs

--- keras_rcnn/layers/__init__.py
+++ keras_rcnn/layers/__init__.py
@@ -3,3 +3,5 @@ from .losses import (RPNClassificationLoss, RPNRegressionLoss)
 from .object_detection import (AnchorTarget, ObjectProposal, ProposalTarget)
 
 from .pooling import RegionOfInterest
+
+from .box_regression import BoxRegression
