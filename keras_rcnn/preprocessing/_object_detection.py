c33bf094894b69cb770cf0488e6458248e10234e
Mihai Morariu <m.a.morariu@delftrobotics.com>
2017-08-23 15:36:13 +0200

    Return metadata.

--- keras_rcnn/preprocessing/_object_detection.py
+++ keras_rcnn/preprocessing/_object_detection.py
@@ -37,8 +37,13 @@ class DictionaryIterator(keras.preprocessing.image.Iterator):
         # Compute and store the target image shape.
         cols, rows, channels          = dictionary[0]["shape"]
         self.image_shape              = (rows, cols, channels)
+
         self.target_shape, self.scale = scale_size(self.image_shape[0:2])
         self.target_shape             = self.target_shape + (self.image_shape[2],)
+
+        # Metadata needs to be computed only once.
+        rows, cols, channels          = self.target_shape
+        self.metadata                 = numpy.array([[rows, cols, self.scale]])
 
         super().__init__(len(self.dictionary), batch_size, shuffle, seed)
 
@@ -48,7 +53,7 @@ class DictionaryIterator(keras.preprocessing.image.Iterator):
             selection, _, batch_size = next(self.index_generator)
 
         # Labels has num_classes + 1 elements, since 0 is reserved for background.
-        num_classes = len(self.classes.keys())
+        num_classes = len(self.classes)
         images      = numpy.zeros((batch_size,) + self.target_shape, dtype=keras.backend.floatx())
         boxes       = numpy.zeros((batch_size, 0, 4),                dtype=keras.backend.floatx())
         labels      = numpy.zeros((batch_size, 0, num_classes + 1),  dtype=numpy.uint8)
@@ -80,7 +85,7 @@ class DictionaryIterator(keras.preprocessing.image.Iterator):
             # Scale the ground truth boxes to the selected image scale.
             boxes[batch_index, :, :4] *= self.scale
 
-        return [images, boxes, labels, self.scale], None
+        return [images, boxes, self.metadata, labels], None
 
 class ObjectDetectionGenerator:
     def flow(self, dictionary, classes):
