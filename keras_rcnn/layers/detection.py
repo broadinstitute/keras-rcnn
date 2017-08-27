790bf1467f50a4d382fda0809fa4fd343d11c544
Allen Goodman <allen.goodman@icloud.com>
2017-08-27 12:31:13 -0400

    detection fixes

--- keras_rcnn/layers/detection.py
+++ keras_rcnn/layers/detection.py
@@ -7,13 +7,19 @@ import keras_rcnn.backend
 class Detection(keras.engine.topology.Layer):
     """
     Get final detections + labels by unscaling back to image space, applying
-    regression deltas
+    regression deltas, choosing box coordinates, and removing extra
+    detections via NMS
 
+    Arguments:
+        threshold: objects with maximum score less than threshold are thrown
+        out
+
+        test_nms: A float representing the threshold for deciding whether
+        boxes overlap too much with respect to IoU
 
     """
 
     def __init__(self, **kwargs):
-
         super(Detection, self).__init__(**kwargs)
 
     def build(self, input_shape):
@@ -28,16 +34,21 @@ class Detection(keras.engine.topology.Layer):
         metadata: image information (1, 3)
 
         # Returns
-        pred_boxes: final predicted boxes of the predicted class (1, N, 4)
+        pred_boxes: predicted boxes (1, N, 4 * classes)
         pred_scores: score distribution over all classes (1, N, classes), note
         the box only corresponds to the most
             probable class, not the other classes
         """
         rois, pred_deltas, pred_scores, metadata = x[0], x[1], x[2], x[3]
+        # rois = rois[0, :, :]
+        # pred_deltas = pred_deltas[0, :, :]
 
         rois = keras.backend.reshape(rois, (-1, 4))
-        pred_deltas = keras.backend.reshape(pred_deltas, (-1, keras.backend.shape(pred_deltas)[2]))
-
+        pred_deltas = keras.backend.reshape(pred_deltas,
+                                            (keras.backend.shape(rois)[0], -1))
+        # pred_scores = keras.backend.reshape(
+        #   pred_scores, (-1, keras.backend.shape(pred_deltas)[2])
+        # )
         # unscale back to raw image space
         boxes = rois / metadata[0][2]
 
@@ -45,14 +56,16 @@ class Detection(keras.engine.topology.Layer):
         pred_boxes = keras_rcnn.backend.bbox_transform_inv(boxes, pred_deltas)
 
         pred_boxes = keras_rcnn.backend.clip(pred_boxes, metadata[0][:2])
-
+        pred_scores = keras.backend.reshape(pred_scores,
+                                            (keras.backend.shape(rois)[0], -1))
         return [
             keras.backend.expand_dims(pred_boxes, 0),
-            pred_scores
+            keras.backend.expand_dims(pred_scores, 0)
         ]
 
     def compute_output_shape(self, input_shape):
-        return [(1, None, 4), (1, None, input_shape[2][2])]
+        return [(1, input_shape[0][0], 4 * input_shape[2][1]),
+                (1, input_shape[0][0], input_shape[2][1])]
 
     def compute_mask(self, inputs, mask=None):
         return 2 * [None]
