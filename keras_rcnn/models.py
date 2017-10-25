cec4734b441f17ee30ad620ed50db84a58849827
Allen Goodman <allen.goodman@icloud.com>
2017-10-25 11:56:25 -0400

    options

--- keras_rcnn/models.py
+++ keras_rcnn/models.py
@@ -84,7 +84,20 @@ def _extract_regions(classes):
     return f
 
 
-def _train(classes):
+def _train(classes, training_options=None):
+    if training_options is None:
+        training_options = {
+            "anchor_target": {
+
+            },
+            "object_proposal": {
+
+            },
+            "proposal_target": {
+
+            }
+        }
+
     def f(inputs):
         options = {
             "activation": "relu",
@@ -98,7 +111,7 @@ def _train(classes):
 
         deltas, scores = _extract_proposals(options)(features)
 
-        all_anchors, rpn_labels, bounding_box_targets = keras_rcnn.layers.AnchorTarget()([scores, bounding_boxes, metadata])
+        all_anchors, rpn_labels, bounding_box_targets = keras_rcnn.layers.AnchorTarget(*training_options["anchor_target"])([scores, bounding_boxes, metadata])
 
         scores_reshaped = keras.layers.Reshape((-1, 2))(scores)
         scores_reshaped = keras.layers.Activation("softmax")(scores_reshaped)
@@ -106,9 +119,9 @@ def _train(classes):
         deltas = keras_rcnn.layers.RPNRegressionLoss(9)([deltas, bounding_box_targets, rpn_labels])
         scores = keras_rcnn.layers.RPNClassificationLoss(9)([scores_reshaped, rpn_labels])
 
-        proposals_ = keras_rcnn.layers.ObjectProposal()([metadata, deltas, scores, all_anchors])
+        proposals_ = keras_rcnn.layers.ObjectProposal(*training_options["object_proposal"])([metadata, deltas, scores, all_anchors])
 
-        proposals, labels_targets, bounding_box_targets = keras_rcnn.layers.ProposalTarget()([proposals_, labels, bounding_boxes])
+        proposals, labels_targets, bounding_box_targets = keras_rcnn.layers.ProposalTarget(*training_options["proposal_target"])([proposals_, labels, bounding_boxes])
 
         deltas, scores = _extract_regions(classes)([features, metadata, proposals])
 
@@ -121,7 +134,28 @@ def _train(classes):
 
 
 class RCNN(keras.models.Model):
-    def __init__(self, image, classes):
+    def __init__(self, image, classes, training_options=None):
+        if training_options is None:
+            training_options = {
+                "anchor_target": {
+                    "allowed_border": 0,
+                    "clobber_positives": False,
+                    "negative_overlap": 0.3,
+                    "positive_overlap": 0.7,
+                },
+                "object_proposal": {
+                    "maximum_proposals": 300,
+                    "minimum_size": 16,
+                    "stride": 16
+                },
+                "proposal_target": {
+                    "fg_fraction": 0.5,
+                    "fg_thresh": 0.7,
+                    "bg_thresh_hi": 0.5,
+                    "bg_thresh_lo": 0.1,
+                }
+            }
+
         inputs = [
             keras.layers.Input((None, 4)),
             image,
@@ -129,7 +163,7 @@ class RCNN(keras.models.Model):
             keras.layers.Input((3,))
         ]
 
-        outputs = _train(classes)(inputs)
+        outputs = _train(classes, training_options)(inputs)
 
         super(RCNN, self).__init__(inputs, outputs)
 
