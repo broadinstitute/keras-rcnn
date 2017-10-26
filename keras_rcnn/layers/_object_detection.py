adaee049380dbe27bf63ef0682b9f24b6b069a9d
Allen Goodman <allen.goodman@icloud.com>
2017-10-26 14:55:20 -0400

    ObjectDetection fixes

--- keras_rcnn/layers/_object_detection.py
+++ keras_rcnn/layers/_object_detection.py
@@ -30,26 +30,28 @@ class ObjectDetection(keras.engine.topology.Layer):
     def call(self, x, training=None, **kwargs):
         """
         # Inputs
-        rois: output of proposal target (1, N, 4)
+        proposals: output of proposal target (1, N, 4)
         deltas: predicted deltas (1, N, 4*classes)
         scores: score distributions (1, N, classes)
         metadata: image information (1, 3)
 
         # Returns
-        pred_boxes: predicted boxes (1, N, 4 * classes)
-        scores: score distribution over all classes (1, N, classes), note
-        the box only corresponds to the most
-            probable class, not the other classes
+
+        bounding_boxes: predicted boxes (1, N, 4 * classes)
+
+        scores: score distribution over all classes (1, N, classes),
+        note the box only corresponds to the most probable class, not the
+        other classes
         """
         def boxes():
-            rois, deltas, scores, metadata = x[0], x[1], x[2], x[3]
+            proposals, deltas, _, metadata = x[0], x[1], x[2], x[3]
 
-            rois = keras.backend.reshape(rois, (-1, 4))
+            proposals = keras.backend.reshape(proposals, (-1, 4))
 
             # unscale back to raw image space
-            boxes = rois / metadata[0][2]     
+            boxes = proposals / metadata[0][2]
             
-            deltas = keras.backend.reshape(deltas, (keras.backend.shape(rois)[0], -1))
+            deltas = keras.backend.reshape(deltas, (keras.backend.shape(proposals)[0], -1))
 
             # Apply bounding-box regression deltas
             pred_boxes = keras_rcnn.backend.bbox_transform_inv(boxes, deltas)
@@ -58,9 +60,11 @@ class ObjectDetection(keras.engine.topology.Layer):
 
             return keras.backend.expand_dims(pred_boxes, 0)
         
-        pred_boxes = keras.backend.in_train_phase(x[1], lambda: boxes(), training=training)
+        bounding_boxes = keras.backend.in_train_phase(x[1], lambda: boxes(), training=training)
 
-        return [pred_boxes, x[2]]
+        scores = x[2]
+
+        return [bounding_boxes, scores]
 
     def compute_output_shape(self, input_shape):
         return [(1, input_shape[0][0], input_shape[1][2]), (1, input_shape[0][0], input_shape[2][2])]
