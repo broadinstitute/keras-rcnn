b81c9e15027b484f5b724fb2b1167dc8916b71e7
Allen Goodman <allen.goodman@icloud.com>
2017-11-03 15:17:48 -0400

    upsample layer

--- keras_rcnn/layers/_upsample.py
+++ keras_rcnn/layers/_upsample.py
@@ -10,23 +10,16 @@ class Upsample(keras.engine.topology.Layer):
         super(Upsample, self).__init__(**kwargs)
 
     def build(self, input_shape):
-
         super(Upsample, self).build(input_shape)
 
     def call(self, inputs, **kwargs):
-        image, target = inputs
+        output, target = inputs
 
         shape = keras.backend.shape(target)
 
-        output_shape = (shape[1], shape[2])
-
-        return keras_rcnn.backend.resize(image, output_shape)
+        return keras_rcnn.backend.resize(output, (shape[1], shape[2]))
 
     def compute_output_shape(self, input_shape):
-        batch_size = input_shape[0][0]
-
         output_shape = input_shape[1][1:3]
 
-        channels = input_shape[0][-1]
-
-        return (batch_size,) + output_shape + (channels,)
+        return (input_shape[0][0],) + output_shape + (input_shape[0][-1],)
