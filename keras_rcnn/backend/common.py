39593d8760759d38e2dd5d5e8202b330bef56648
jhung0 <jyhung@broadinstitute.org>
2018-01-24 15:42:12 -0500

    adjust bbox transform function

--- keras_rcnn/backend/common.py
+++ keras_rcnn/backend/common.py
@@ -26,13 +26,13 @@ def anchor(base_size=16, ratios=None, scales=None):
 
 def bbox_transform(ex_rois, gt_rois):
     """Compute bounding-box regression targets for an image."""
-    ex_widths = ex_rois[:, 2] - ex_rois[:, 0] + 1.0
-    ex_heights = ex_rois[:, 3] - ex_rois[:, 1] + 1.0
+    ex_widths = ex_rois[:, 2] - ex_rois[:, 0]
+    ex_heights = ex_rois[:, 3] - ex_rois[:, 1]
     ex_ctr_x = ex_rois[:, 0] + 0.5 * ex_widths
     ex_ctr_y = ex_rois[:, 1] + 0.5 * ex_heights
 
-    gt_widths = gt_rois[:, 2] - gt_rois[:, 0] + 1.0
-    gt_heights = gt_rois[:, 3] - gt_rois[:, 1] + 1.0
+    gt_widths = gt_rois[:, 2] - gt_rois[:, 0]
+    gt_heights = gt_rois[:, 3] - gt_rois[:, 1]
     gt_ctr_x = gt_rois[:, 0] + 0.5 * gt_widths
     gt_ctr_y = gt_rois[:, 1] + 0.5 * gt_heights
 
