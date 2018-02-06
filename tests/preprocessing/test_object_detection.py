eacbf9569eee076986c461e81b1f73609198621d
jhung0 <jyhung@broadinstitute.org>
2018-02-06 10:31:38 -0500

    add preprocessing tests

--- tests/preprocessing/test_object_detection.py
+++ tests/preprocessing/test_object_detection.py
@@ -1,37 +1,31 @@
 import numpy
 
 import keras_rcnn.preprocessing._object_detection
-import keras_rcnn.datasets.malaria
+import keras_rcnn.datasets.dsb2018
 import keras_rcnn.models
 import keras
-
-
-def test_scale_shape():
-    min_size = 200
-    max_size = 300
-    size = (600, 1000)
-
-    size, scale = keras_rcnn.preprocessing._object_detection.scale_size(
-        size, min_size, max_size
-    )
-
-    expected = (180, 300)
-    numpy.testing.assert_equal(size, expected)
-
-    expected = 0.3
-
-    assert numpy.isclose(scale, expected)
-
+import skimage.io
 
 class TestObjectDetectionGenerator:
-    def test_flow(self):
+    def test_flow_from_dictionary(self):
         classes = {
-            "rbc": 1,
-            "not":2
+            "nucleus": 1
         }
 
-        training, _, _ = keras_rcnn.datasets.malaria.load_data()
+        training, _ = keras_rcnn.datasets.dsb2018.load_data()
 
         generator = keras_rcnn.preprocessing.ObjectDetectionGenerator()
 
-        generator = generator.flow_from_dictionary(training, classes, target_shape=(448, 448), scale=1)
+        generator = generator.flow_from_dictionary(training, classes, target_size=(448, 448))
+
+        generator.next()
+
+    def test_standardize(self):
+
+        training, _ = keras_rcnn.datasets.dsb2018.load_data()
+
+        generator = keras_rcnn.preprocessing.ObjectDetectionGenerator()
+
+        image = skimage.io.imread(training[0]['image']['pathname'])
+
+        generator.standardize(image)
