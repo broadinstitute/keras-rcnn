fe732300849fef48583f989d05c7bf83a2f5ae5f
Allen Goodman <allen.goodman@icloud.com>
2018-02-09 16:17:06 -0500

    losses

--- keras_rcnn/layers/losses/_mask_rcnn.py
+++ keras_rcnn/layers/losses/_mask_rcnn.py
@@ -2,20 +2,18 @@
 
 import keras.backend
 import keras.layers
+import tensorflow
 
 
 class MaskRCNN(keras.layers.Layer):
-    def __init__(self, threshold=0.5, **kwargs):
-        self.threshold = threshold
-
+    def __init__(self, **kwargs):
         super(MaskRCNN, self).__init__(**kwargs)
 
     def call(self, inputs, training=None, **kwargs):
-        target_bounding_boxes, output_bounding_boxes, target_masks, output_masks = inputs
+        target_categories, target_masks, output_masks = inputs
 
         loss = self.compute_mask_loss(
-            target_bounding_boxes,
-            output_bounding_boxes,
+            target_categories,
             target_masks,
             output_masks
         )
@@ -24,68 +22,22 @@ class MaskRCNN(keras.layers.Layer):
 
         return output_masks
 
-    @staticmethod
-    def intersection_over_union(a, b):
-        # TODO: Write `split` Keras backend function, e.g.
-        #     keras.backend.split(a, 4, axis=1)
-        a_x1, a_y1, b_x1, b_y1 = a[:, 0:1], a[:, 1:2], a[:, 2:3], a[:, 3:]
-
-        # TODO: Write `split` Keras backend function, e.g.
-        #     keras.backend.split(b, 4, axis=1)
-        a_x2, a_y2, b_x2, b_y2 = b[:, 0:1], b[:, 1:2], b[:, 2:3], b[:, 3:]
-
-        x_intersection_1 = keras.backend.maximum(a_x1, keras.backend.transpose(a_x2))
-        y_intersection_1 = keras.backend.maximum(a_y1, keras.backend.transpose(a_y2))
-
-        x_intersection_2 = keras.backend.minimum(b_x1, keras.backend.transpose(b_x2))
-        y_intersection_2 = keras.backend.minimum(b_y1, keras.backend.transpose(b_y2))
-
-        a = keras.backend.maximum(x_intersection_2 - x_intersection_1 + 1, 0)
-        b = keras.backend.maximum(y_intersection_2 - y_intersection_1 + 1, 0)
-
-        intersection = a * b
-
-        bounding_boxes_a_area = (b_x1 - a_x1 + 1) * (b_y1 - a_y1 + 1)
-        bounding_boxes_b_area = (b_x2 - a_x2 + 1) * (b_y2 - a_y2 + 1)
-
-        union = (bounding_boxes_a_area + keras.backend.transpose(bounding_boxes_b_area)) - intersection
-
-        return keras.backend.maximum(intersection / union, 0)
-
-    def categorical_crossentropy(self, target, output):
-        output = output + keras.backend.epsilon()
-
-        output = keras.backend.transpose(keras.backend.log(output))
-
-        return -keras.backend.dot(target, output)
-
     def compute_mask_loss(
             self,
-            target_bounding_boxes,
-            output_bounding_boxes,
+            target_categories,
             target_masks,
             output_masks
     ):
-        target_bounding_boxes = keras.backend.squeeze(target_bounding_boxes, axis=0)
-        output_bounding_boxes = keras.backend.squeeze(output_bounding_boxes, axis=0)
+        target_categories = keras.backend.argmax(target_categories)
 
-        # TODO: simplify (this is just a fancy method to find width * height)
-        index = keras.backend.prod(keras.backend.shape(target_masks)[1:])
+        target_categories = keras.backend.squeeze(target_categories, 0)
 
-        target_masks = keras.backend.reshape(target_masks, [-1, index])
-        output_masks = keras.backend.reshape(output_masks, [-1, index])
+        # FIXME: submit patch to Keras that adds axis paramter to `keras.backend.gather`.
+        output_masks = tensorflow.gather(output_masks, target_categories, axis=-1)[:, :, :, :, 0]
 
-        score = self.intersection_over_union(
-            target_bounding_boxes,
-            output_bounding_boxes
-        )
+        loss = keras.backend.binary_crossentropy(target_masks, output_masks)
 
-        a = self.categorical_crossentropy(target_masks, output_masks)
-
-        b = keras.backend.greater(score, self.threshold)
-        b = keras.backend.cast(b, dtype=keras.backend.floatx())
-
-        return keras.backend.mean(a * b) + keras.backend.mean(1 - b)
+        return keras.backend.mean(loss)
 
     def compute_output_shape(self, input_shape):
-        return input_shape[3]
+        return input_shape[1]
