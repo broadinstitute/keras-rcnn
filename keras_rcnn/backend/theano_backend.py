4d7763a0de0ae7b03f58c683c4849f897cabe6c8
Allen Goodman <allen.goodman@icloud.com>
2018-02-20 14:33:03 -0500

    backend scaffolding

---
+++ keras_rcnn/backend/theano_backend.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+
+def argsort(x, **kwargs):
+    raise NotImplementedError
+
+
+def meshgrid(*nargs, **kwargs):
+    raise NotImplementedError
+
+
+def pad(x, pad_width, mode, **kwargs):
+    raise NotImplementedError
+
+
+def shuffle(x):
+    raise NotImplementedError
+
+
+def squeeze(x, axis=None):
+    raise NotImplementedError
+
+
+def transpose(x, axes=None):
+    raise NotImplementedError
+
+
+def unique(x, return_index=False, **kwargs):
+    raise NotImplementedError
+
+
+def where(condition, x=None, y=None):
+    raise NotImplementedError
