54cdf43ada9a23072111c6fe707ca88a384e5f9e
Allen Goodman <allen.goodman@icloud.com>
2018-02-23 13:40:38 -0500

    show_bounding_boxes function

---
+++ keras_rcnn/util/_visualization.py
@@ -0,0 +1,40 @@
+import matplotlib.cm
+import matplotlib.patches
+import matplotlib.pyplot
+import numpy
+
+
+def _get_cmap(n, name="hsv"):
+    return matplotlib.cm.get_cmap(name, n)
+
+
+# TODO: commit upstream to `scikit-image`.
+def show_bounding_boxes(image, bounding_boxes, categories=None):
+    _, axis = matplotlib.pyplot.subplots(1)
+
+    n = bounding_boxes.shape[0]
+
+    if categories is not None:
+        assert bounding_boxes.shape[0] == categories.shape[0]
+
+        categories = categories.reshape((-1, 1))
+    else:
+        categories = numpy.zeros((bounding_boxes.shape[0], 1))
+
+    axis.imshow(image)
+
+    colormap = _get_cmap(n)
+
+    for bounding_box, category in zip(bounding_boxes, categories):
+        rectangle = matplotlib.patches.Rectangle(
+            [
+                bounding_box[1],
+                bounding_box[0]
+            ],
+            bounding_box[3] - bounding_box[1],
+            bounding_box[2] - bounding_box[0],
+            edgecolor=colormap(category[0]),
+            facecolor="none"
+        )
+
+        axis.add_patch(rectangle)
