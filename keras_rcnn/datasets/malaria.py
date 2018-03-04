46f89ccec0238b0a1078a43c7d441d28c7bbe80c
Allen Goodman <allen.goodman@icloud.com>
2018-03-04 18:18:26 -0500

    rename

--- keras_rcnn/datasets/malaria.py
+++ keras_rcnn/datasets/malaria.py
@@ -6,6 +6,7 @@ import keras_rcnn.datasets
 def load_data():
     """
     Load Hung, et al.’s malaria dataset.
+
     Hung, et al.’s malaria dataset is a collection of blood smears from
     multiple patients captured by a variety of microscopes. Images are
     accompanied by bounding boxes that capture each cell’s location and
