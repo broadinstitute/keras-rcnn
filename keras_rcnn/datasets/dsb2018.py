fdb3e2992f0127d72966dfee52f9ad440bf8e7d3
Allen Goodman <allen.goodman@icloud.com>
2018-03-20 15:38:59 -0400

    DSB2018

--- keras_rcnn/datasets/dsb2018.py
+++ keras_rcnn/datasets/dsb2018.py
@@ -1,7 +1,46 @@
 # -*- coding: utf-8 -*-
 
-import keras_rcnn.datasets
+import json
+import os.path
+
+import keras.utils.data_utils
 
 
 def load_data():
-    return keras_rcnn.datasets.load_data("DSB2018")
+    name = "DSB2018"
+
+    origin = "http://keras-rcnn.storage.googleapis.com/{}.tar.gz".format(name)
+
+    pathname = keras.utils.data_utils.get_file(
+        fname=name,
+        origin=origin,
+        untar=True
+    )
+
+    filename = os.path.join(pathname, "training.json")
+
+    if os.path.exists(filename):
+        with open(filename) as data:
+            training = json.load(data)
+    else:
+        training = []
+
+    for dictionary in training:
+        dictionary["image"]["pathname"] = pathname + dictionary["image"]["pathname"]
+
+        for instance in dictionary["objects"]:
+            instance["mask"]["pathname"] = pathname + instance["mask"]["pathname"]
+
+    filename = os.path.join(pathname, "test.json")
+
+    if os.path.exists(filename):
+        with open(filename) as data:
+            test = json.load(data)
+    else:
+        test = []
+
+    for dictionary in test:
+        dictionary["image"]["pathname"] = pathname + dictionary["image"]["pathname"]
+
+        for instance in dictionary["objects"]:
+            instance["mask"]["pathname"] = pathname + instance["mask"]["pathname"]
