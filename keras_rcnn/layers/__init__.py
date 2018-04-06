a4dba33a6fcc57b1cc34a614cc8a8928c1503dea
Allen Goodman <allen.goodman@icloud.com>
2018-04-06 10:25:52 -0400

    exposed layer

--- keras_rcnn/layers/__init__.py
+++ keras_rcnn/layers/__init__.py
@@ -2,6 +2,8 @@ from .losses import (RCNN, RPN)
 
 from .object_detection import (Anchor, ObjectProposal, ProposalTarget)
 
+from ._gradient_reversal import GradientReversal
+
 from ._pooling import RegionOfInterest
 
 from ._object_detection import ObjectDetection
